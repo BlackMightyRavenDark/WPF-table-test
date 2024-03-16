9fc1b47863fa3ad9f660d034dc845f7bbf2b8b2d
BlackRain <masaraksh86@mail.ru>
2024-03-16 12:47:55 +0700

    Добавьте файлы проекта.

---
+++ WPF ListView/Model.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace WPF_ListView
+{
+    internal class Model : INotifyPropertyChanged
+    {
+        public string IP { get; set; }
+        public string Ping { get; set; }
+        public string DNS { get; set; }
+        public string MAC { get; set; }
+        public string Manufacturer { get; set; }
+
+        private Brush _backgroundColor;
+        public Brush BackgroundColor
+        {
+            get => _backgroundColor;
+            set
+            {
+                _backgroundColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
