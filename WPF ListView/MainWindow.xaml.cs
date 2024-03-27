2a1fa05dccc7a447cdecfa5e04d193023deed36b
BlackRain <masaraksh86@mail.ru>
2024-03-27 13:42:10 +0700

    CR LF

--- WPF ListView/MainWindow.xaml.cs
+++ WPF ListView/MainWindow.xaml.cs
@@ -1,12 +1,108 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
 
 namespace WPF_ListView
 {
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Model> items = new ObservableCollection<Model>();
+        private GridViewColumnHeader _lastHeaderClicked = null;
+        private ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listView.ItemsSource = items;
+            //listView.Items.GroupDescriptions.Add(new PropertyGroupDescription("State"));
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listView.ItemsSource);
+            PropertyGroupDescription groupDescription = new PropertyGroupDescription("State");
+            view.GroupDescriptions.Add(groupDescription);
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += (s, a) =>
+            {
+                if (items.Count > 0)
+                {
+                    Random random = new Random();
+                    items[random.Next(items.Count)].State = random.Next(2);
+                }
+            };
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Start();
+        }
+
+        private void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    string sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+                    if (sortBy.Contains("/")) { sortBy = "Name"; }
+
+                    Sort(sortBy, direction);
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(listView.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int c = items.Count;
+            Random random = new Random();
+            for (int i = 0; i < 200; ++i)
+            {
+                Model model = new Model() { Index = i + c, State = random.Next(2) };
+                items.Add(model);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
--- WPF ListView/Model.cs
+++ WPF ListView/Model.cs
@@ -9,66 +9,16 @@ using System.Windows.Input;
 
 namespace WPF_ListView
 {
-    internal class Model : INotifyPropertyChanged, ICommand
+    internal class Model : INotifyPropertyChanged
     {
-        public string IP { get; set; }
-        public string Ping { get; set; }
-        public string DNS { get; set; }
-        public string MAC { get; set; }
-        public string Manufacturer { get; set; }
+        public int Index { get => _index; set { _index = value; OnPropertyChanged(); } }
+        public int State { get => _state; set { _state = value; OnPropertyChanged(); } }
 
-        private Brush _backgroundColor;
-        public Brush BackgroundColor
-        {
-            get => _backgroundColor;
-            set
-            {
-                _backgroundColor = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private CollectionViewSource cvs = new CollectionViewSource();
-        private ObservableCollection<Model> items = new ObservableCollection<Model>();
-
-        public ICollectionView View { get => cvs.View; }
-
-        public event EventHandler CanExecuteChanged;
-        public bool CanExecute(object parameter) => true;
-        public void Execute(object parameter)
-        {
-            switch (parameter.ToString())
-            {
-                case "ButtonAdd_Click":
-                    for (int i = 0; i < 20; ++i)
-                    {
-                        Model model = new Model() { IP = "1.1.1.1", Ping = "30ms", DNS = "XYZ", MAC = "2F:3C:5F:41:F9", Manufacturer = "Intel" };
-                        items.Add(model);
-                    }
-                    cvs.Source = items;
-                    OnPropertyChanged(nameof(View));
-                    break;
-
-                case "ButtonColorize_Click":
-                    RandomizeColors();
-                    break;
-            }
-        }
-
-        private void RandomizeColors()
-        {
-            Random random = new Random();
-            foreach (Model model in items)
-            {
-                Type brushesType = typeof(Brushes);
-                PropertyInfo[] properties = brushesType.GetProperties();
-                int randomInt = random.Next(properties.Length);
-                model.BackgroundColor = (Brush)properties[randomInt].GetValue(null, null);
-            }
-        }
+        private int _index;
+        private int _state;
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propName = "") =>
+        protected void OnPropertyChanged([CallerMemberName] string propName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
     }
 }
